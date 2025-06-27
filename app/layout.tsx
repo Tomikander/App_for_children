2af6e4c741296eb84307bf7626e5d50d120f2dd4
tomikander <andersontomik@gmail.com>
2025-06-28 00:08:02 +0300

    feat/install husky_pre_commit_and_lint_starged

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Math app for kids",
-  description: "app for kids",
+  title: 'Math app for kids',
+  description: 'app for kids',
 };
 
 export default function RootLayout({
--- app/lib/resultsStore.ts
+++ app/lib/resultsStore.ts
@@ -1,14 +1,13 @@
 import { create } from 'zustand';
 
 interface ResultsState {
-  resultsAmountToGenerate: number; 
-  setResultsAmountToGenerate: (amount: number) => void; 
+  resultsAmountToGenerate: number;
+  setResultsAmountToGenerate: (amount: number) => void;
 }
 
 export const useResultsStore = create<ResultsState>((set) => ({
-  resultsAmountToGenerate: 0, 
+  resultsAmountToGenerate: 0,
 
   setResultsAmountToGenerate: (amount: number) =>
     set({ resultsAmountToGenerate: amount }),
 }));
-
