e4aded8f6a1da929040f9f534a457450f6d09274
tomikander <andersontomik@gmail.com>
2025-06-29 21:25:24 +0300

    feat/install_and_implement_mui_react_library

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./lib/theme";
+import { PropsWithChildren } from "react";
+import  "./styles/home.module.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: "--font-roboto",
 });
 
 export const metadata: Metadata = {
@@ -16,15 +17,14 @@ export const metadata: Metadata = {
   description: "app for kids",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout(props: PropsWithChildren) {
+  const { children } = props;
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="en" className={roboto.variable}>
+      <body>
+        <ThemeProvider theme={theme}>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
