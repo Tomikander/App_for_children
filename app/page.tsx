04ef1e041125f9f4c2b735acf2b9faa880633162
Tomikander <167113924+Tomikander@users.noreply.github.com>
2025-07-03 15:47:04 +0300

    I'm all done, corrected the notes in the comments

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,6 @@ export default function Home() {
     }
   };
 
-
 const handleSliderChange = (_event: Event, value: number | number[]) => {
   if (typeof value === "number") {
     setResultsAmountToGenerate(value);
