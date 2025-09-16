cb7c3583a037fd2652fe3d5ba8c888d04b0d9323
tomikander <andersontomik@gmail.com>
2025-09-16 20:39:51 +0300

    created_two_fields_minimum_and_maximum_which_can_be_edited_and_there_is_also_auto-correction_of_the_minimum_number v2

--- app/lib/hooks/useGenerateExpressions.ts
+++ app/lib/hooks/useGenerateExpressions.ts
@@ -1,13 +1,19 @@
 import { generateAdditionExpressions } from '../helpers/generateExpressions';
 import { useMaxNumber } from './useMaxNumber';
 import { useResultsStore } from '../resultsStore';
+import { useSettingsStore } from '../settingsStore/settingsStore';
 
 export const useGenerateExpressions = () => {
   const { maxAdditionSum } = useMaxNumber();
   const { resultsAmountToGenerate } = useResultsStore();
+  const { minAdditionOperand } = useSettingsStore();
 
   const createMathTasks = () => {
-    return generateAdditionExpressions(maxAdditionSum, resultsAmountToGenerate);
+    return generateAdditionExpressions(
+      maxAdditionSum,
+      resultsAmountToGenerate,
+      minAdditionOperand
+    );
   };
 
   return { createMathTasks };
